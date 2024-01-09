7f009380c8e9430118c6c530f81786fbed23e49e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2024-01-09 18:49:01 +0300

    added rails api to react-full calendar

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,27 @@
+class Api::EventsController < ApplicationController
+     
+    def index
+        @events = Event.all
+       render json: @events
+      end
+    
+     
+   
+      def create
+        @event = Event.new(event_params)
+        if @event.save
+            render json: @event, status: :created, location: @user
+          else
+            render json: @event.errors, status: :unprocessable_entity
+          end
+        
+      
+      end
+   
+      
+    private
+  
+        def event_params
+          params.permit(:title, :start, :end)
+        end
+end
